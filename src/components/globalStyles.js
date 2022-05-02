b457bbb1dc8bf9382663e263baf198333fc0f166
iepppop <iepppop@gmail.com>
2022-05-02 17:34:48 +0900

    m

--- src/components/globalStyles.js
+++ src/components/globalStyles.js
@@ -4,13 +4,13 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyles = createGlobalStyle`
 @font-face {
     font-family: 'Pretendard';
-    src: url('../fonts/Pretendard-Thin.otf');
+    src: url('./fonts/Pretendard-Thin.otf');
   }
   
   
   @font-face {
     font-family: "JejuMyeongjo";
-    src: url('../fonts/JejuMyeongjo.ttf');
+    src: url('./fonts/JejuMyeongjo.ttf');
   }
     body{
         background:${({ theme }) => theme.body};
@@ -23,6 +23,7 @@ export const GlobalStyles = createGlobalStyle`
         *{
             padding:0; 
             margin:0;
+            font-family:'Pretendard';
     }
 `;
 
