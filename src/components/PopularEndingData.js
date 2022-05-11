7d4a2239664053c679a40d990b8d908bbaa94993
iepppop <iepppop@gmail.com>
2022-05-11 13:15:46 +0900

    m

--- src/components/PopularEndingData.js
+++ src/components/PopularEndingData.js
@@ -4,59 +4,69 @@ export const popularEnding = [
         color: '#8c6d3f',
         img:'https://blog.kakaocdn.net/dn/bUOvnd/btrBi73iD3f/BwbukZ70CK47PCB17lgK6K/img.png',
         logo:'/image/ki01.png',
+        author:'쿠기'
     },
     {
         name: '불멸의 날들',
         color: '#d8323e',
         img:'https://blog.kakaocdn.net/dn/egtlvz/btrBiqbbUka/K35K5KZktexwedOPBuSMk1/img.png',
         logo:'/image/ki02.png',
+        author:'허긴개'
     },
     {
         name: '골든아워',
         color: '#657aab',
         img:'https://blog.kakaocdn.net/dn/cpYHT4/btrBiHRkLnV/WueeLbDTIcDNg2xoLimf80/img.png',
         logo:'/image/ki03.png',
+        author:'박수빈'
     },
     {
         name: '러브오어헤이트',
         color: '#a67273',
         img:'https://blog.kakaocdn.net/dn/bHzUlP/btrBik9QfZb/dmErUw3ZRSBp4Z4eCOZBZK/img.png',
         logo:'/image/ki04.png',
+        author:'영하박담'
     },
     {
         name: '나는너의귀여운카피캣',
         color: '#efd75b',
         img:'https://blog.kakaocdn.net/dn/kElbv/btrBhQ98Wcn/a62c0okMqe8rp3yczZTLw1/img.png',
         logo:'/image/ki05.png',
+        author:'마롭믹'
     },
     {
         name: '도수풀',
         color: '#ffa834',
         img:'https://blog.kakaocdn.net/dn/bZ5XOh/btrBvYFjDAb/AgVqGZHVtlgQamp0OKH0h0/img.png',
         logo:'/image/ki06.png',
+        author:'신'
     },
     {
         name: '이토록 다채로운 핏빛',
         color: '#ed7088',
         img:'https://blog.kakaocdn.net/dn/bgKhVN/btrBtDaRzxN/G1A30qBsM95uDS6hoNy340/img.png',
         logo:'/image/ki07.png',
+        author:'므앵갱'
     },
     {
         name: '바람',
         color: '#fc3f0a',
         img:'https://blog.kakaocdn.net/dn/Ad7Lb/btrBsEUNVKJ/lcresxHZ6opzv2Zkx4qIsk/img.png',
         logo:'/image/ki08.png',
+        author:'반매'
     },
     {
         name: '호박장군',
         color: '#544460',
         img:'https://blog.kakaocdn.net/dn/bvyMoW/btrBxaZS5NC/RRwTgvhKHOHAHc88up2pr1/img.png',
         logo:'/image/ki09.png',
+        author:'이운'
     },
     {
         name: '어둠이스러지는꽃',
         color: '#000000',
         img:'https://blog.kakaocdn.net/dn/bRYfWH/btrBx0JsOXF/yVFKNjwBbYQ9Bces2GTTa1/img.png',
         logo:'/image/ki10.png',
+        author:'므앵갱'
     },
 ]
